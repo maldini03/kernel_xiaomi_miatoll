wilfridd/kernel_xiaomi_sm6250

drivers: Fix mismerge from ACK
* Should have done a long time ago

Signed-off-by: clarencelol <clarencekuiek@icloud.com>

drivers/clk/clk.c
@@ -69,6 +69,13 @@ static struct hlist_head *all_lists[] = {
 	NULL,
 };
 
+#ifdef CONFIG_DEBUGFS
+static struct hlist_head *orphan_list[] = {
+	&clk_orphan_list,
+	NULL,
+};
+#endif
+
 /***    private data structures    ***/
 
 struct clk_core {
@@ -2698,11 +2705,6 @@ static int inited = 0;
 static u32 debug_suspend;
 static DEFINE_MUTEX(clk_debug_lock);
 static HLIST_HEAD(clk_debug_list);
-
-static struct hlist_head *orphan_list[] = {
-	&clk_orphan_list,
-	NULL,
-};
 
 static void clk_state_subtree(struct clk_core *c)
 {

maldini03/kernel_xiaomi_miatoll

drivers/clk/clk.c
@@ -69,6 +69,13 @@ static struct hlist_head *all_lists[] = {
 	NULL,
 };
 
+#ifdef CONFIG_DEBUG_FS
+static struct hlist_head *orphan_list[] = {
+	&clk_orphan_list,
+	NULL,
+};
+#endif
+
 /***    private data structures    ***/
 
 struct clk_core {
@@ -2698,11 +2705,6 @@ static int inited = 0;
 static u32 debug_suspend;
 static DEFINE_MUTEX(clk_debug_lock);
 static HLIST_HEAD(clk_debug_list);
-
-static struct hlist_head *orphan_list[] = {
-	&clk_orphan_list,
-	NULL,
-};
 
 static void clk_state_subtree(struct clk_core *c)
 {